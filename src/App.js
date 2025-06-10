65adafc674d05e8abf4fa65721ddd97f62622d8b
Ngoc <nsngoc131204@gmail.com>
2025-06-10 18:53:01 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="earth">
+      <div>Hello World</div>
+      <div className="logo-container">
+      <img className='logo' src='https://upload.wikimedia.org/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17.jpg'></img>
+      </div>
+      <p>This is my first react code</p>
+    </div>
+  );
+}
+
+export default App;
